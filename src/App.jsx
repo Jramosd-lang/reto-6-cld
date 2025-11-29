5a77b540629b1afdd140181d53e0fe654ee7ad07
jr10p9 <chessjr10p9@gmail.com>
2025-11-29 15:53:41 -0500

    correccion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Proveedores from './Componentes'
 import CasoCritico from './Componentes/caso critico'  
-import TerraformExplicacion from './Componentes/terraform explicacion'
+import TerraformExplicacion from './Componentes/terraform Explicacion/index.jsx'
 import './App.css'
 
 
